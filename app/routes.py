cd7d1d7c54044e37615f3cdbb51d1228e95e3736
DaviBMMGallo <davimattosgallo@gmail.com>
2024-02-17 16:33:30 -0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -11,29 +11,29 @@ def create_server(config):
     app.register_blueprint(teste.get_blueprint(), url_prefix=PREFX_API)
     db = connection.getConnection(config.PSQL_SETTINGS)
     cur = db.cursor()
-    try:
-        # Define the INSERT INTO statement
-        sql = """
-            INSERT INTO Produto (nome_produto, descricao, valor, desconto)
-            VALUES (%s, %s, %s, %s)
-        """
+    # try:
+    #     # Define the INSERT INTO statement
+    #     sql = """
+    #         INSERT INTO Produto (nome_produto, descricao, valor, desconto)
+    #         VALUES (%s, %s, %s, %s)
+    #     """
 
-        # Define the values to be inserted
-        values = ('Product B', 'Description of Product B', 100.0, 0.1)
+    #     # Define the values to be inserted
+    #     values = ('Product B', 'Description of Product B', 100.0, 0.1)
 
-        # Execute the INSERT INTO statement
-        cur.execute(sql, values)
+    #     # Execute the INSERT INTO statement
+    #     cur.execute(sql, values)
 
-        # Commit the transaction
-        db.commit()
+    #     # Commit the transaction
+    #     db.commit()
 
-        print("Data inserted into Produto table successfully!")
+    #     print("Data inserted into Produto table successfully!")
 
-    except psycopg2.Error as e:
-        # Rollback the transaction in case of error
-        db.rollback()
+    # except psycopg2.Error as e:
+    #     # Rollback the transaction in case of error
+    #     db.rollback()
 
-        print("Error inserting data into Produto table:", e)
+    #     print("Error inserting data into Produto table:", e)
 
 
     return app 
