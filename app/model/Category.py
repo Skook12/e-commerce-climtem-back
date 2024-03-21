4fe9d18026cda24a5d140c90129c3ec1f59ffbf7
markosleal13 <80006143+markosleal13@users.noreply.github.com>
2024-03-20 23:09:28 -0300

    Add files via upload

--- app/model/Category.py
+++ app/model/Category.py
@@ -5,3 +5,10 @@ class Category:
     id: int = field(init=False)
     product_id: int = field(init=False)
     name: str
+    
+    def load(self):
+        return (
+            self.id,
+            self.product_id,
+            f'{self.name}'
+        )
