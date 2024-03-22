25908553f9737d6180c70218a172d6e796ed7239
markosleal13 <80006143+markosleal13@users.noreply.github.com>
2024-03-21 23:56:01 -0300

    Update Product and Stock

--- app/model/Stock.py
+++ app/model/Stock.py
@@ -3,8 +3,14 @@ from datetime import datetime
 
 @dataclass
 class Stock:
-    id: int = field(init=False)
     product_id: int = field(init=False)
     amount: str
     created_at: datetime
     updated_at: datetime
+    def load(self):
+        return (
+            self.amount,
+            self.datetime,
+            f'{self.created_at}',
+            f'{self.updated_at}'
+        )
