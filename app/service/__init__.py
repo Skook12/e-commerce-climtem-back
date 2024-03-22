25ac75529218bf280790f583e7567b00ce4a76cb
markosleal13 <80006143+markosleal13@users.noreply.github.com>
2024-03-21 23:58:10 -0300

    Update Product and Stock

--- app/service/__init__.py
+++ app/service/__init__.py
@@ -3,12 +3,16 @@ from .address import AddressService
 from .brand import BrandService
 from .category import CategoryService
 from .order import OrderService
+from .stock import StockServices
+from .product import ProductService
 
 __all__ = [
     'UserService',
     'AddressService',
     'BrandService',
     'CategoryService',
-    'OrderService'
+    'OrderService',
+    'StockServices',
+    'ProductService'
     
 ]
