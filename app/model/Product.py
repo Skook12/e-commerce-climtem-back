86b2c24d826c0ae964951f0423c88a11a31e7714
DaviBMMGallo <32436536+DavibmmGallo@users.noreply.github.com>
2024-03-23 09:34:52 -0300

    Product resource fixed (#11)
    - Changed dataclass and endpoints, that previously were with order attributes

--- app/model/Product.py
+++ app/model/Product.py
@@ -2,13 +2,17 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Product:
-    id: int = field(init=False)
+    brand_id: int
+    category_id: int
     name: str
     description: str
     value: float
     discount: float
+
     def load(self):
         return (
+            self.brand_id,
+            self.category_id,
             self.name,
             self.description,
             self.value,
