23256edda9afe5cb464a36fc6dfb3d134096db21
DaviBMMGallo <davimattosgallo@gmail.com>
2024-04-06 09:11:55 -0300

    Update Create_Table_Ecommerce.sql
    Translating rest of attributes to en

--- Create_Table_Ecommerce.sql
+++ Create_Table_Ecommerce.sql
@@ -3,20 +3,20 @@
 
 CREATE TABLE UserTable (
     ID_User SERIAL PRIMARY KEY,
-    nome VARCHAR(255),
+    name VARCHAR(255),
     email VARCHAR(255),
-    senha VARCHAR(255),
-    telefone BIGINT
+    password VARCHAR(255),
+    phone BIGINT
 );
 
 /* Tabela de criacao do endereco */
 CREATE TABLE User_Address (
     ID_User_Address SERIAL PRIMARY KEY,
     ID_User INT REFERENCES UserTable(ID_User),
-    numero INT,
-    complemento VARCHAR(255),
+    num INT,
+    complement VARCHAR(255),
     cep VARCHAR(10),
-    cidade VARCHAR(255)
+    city VARCHAR(255)
 );
 
 /* Tabela de criacao da marca do produto */
@@ -36,17 +36,17 @@ CREATE TABLE Product (
    ID_Product SERIAL PRIMARY KEY,
    ID_Brand INT REFERENCES Brand(brand_id),
    ID_Category INT REFERENCES Category(category_id), 
-   nome_produto VARCHAR(255),
-   descricao TEXT,
-   valor DECIMAL,
-   desconto DECIMAL
+   name VARCHAR(255),
+   description TEXT,
+   value DECIMAL,
+   discount DECIMAL
 );
 
 /* Tabela de criacao do pedido */
 CREATE TABLE Order (
   ID_Order SERIAL PRIMARY KEY, 
   ID_User INT REFERENCES UserTable(ID_User), 
-  data_compra TIMESTAMP, 
+  buy_date TIMESTAMP, 
   status VARCHAR (50)
 );
 
@@ -54,23 +54,23 @@ CREATE TABLE Order (
 CREATE TABLE Products_Order (
   ID_Products_Order SERIAL PRIMARY KEY, 
   ID_Order INT REFERENCES Order(ID_Order), 
-  quantidade INT, 
-  valor_compra DECIMAL
+  quantity INT, 
+  total_bought DECIMAL
 ); 
 
 /* Tabela de criacao do pagamento */
 CREATE TABLE User_Pagamento(
 	ID_User_Pagamento SERIAL PRIMARY KEY,	
 	ID_Oder INT REFERENCES Order(ID_Order),	
-	tipo_pagamento	VARCHAR (50),	
-	prazo	DATE
-);	
+	payment_type	VARCHAR (50),	
+	expiration DATE
+);
 
 /* Tabela de criacao do estoque do produto */
 CREATE TABLE Product_Stock(
 	ID_Product_Stock	SERIAL	PRIMARY	KEY,	
 	ID_Product	INT	REFERENCES	Product(ID_Product),	
-	quantidade	INT,	
-	data_cadastro	TIMESTAMP,	
-	modificado_em	TIMESTAMP
+	quantity	INT,	
+	register_date	TIMESTAMP,	
+	modified_date	TIMESTAMP
 );
