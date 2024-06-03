f31cee62c8d26e1ca6a684d897fd9139f7befd64
DaviBMMGallo <davimattosgallo@gmail.com>
2024-06-03 12:19:18 -0300

    Added put user route

--- app/resource/user.py
+++ app/resource/user.py
@@ -15,21 +15,55 @@ from app.model import (
 from http import HTTPStatus
 
 
-def get_blueprint(srvc: UserService, addrsrvc: AddressService, config) -> Blueprint:
+def get_blueprint(srvc: UserService, addrsrvc: AddressService) -> Blueprint:
     bp = Blueprint("User", __name__)
-
-    @bp.get('/users')
-    def getUsers():
-        #TODO: REMOVE route
-        """Development only"""
-        r = srvc.select()
-        return jsonify(r)
 
     @bp.get('/users/<int:id>')
     @token_required(id)
     def getUserbyid(id):
         r = srvc.select(f'u JOIN User_Address a ON a.ID_User = u.ID_User WHERE u.ID_User = {id}')
         return jsonify(r)
+
+    @bp.put('/users/<int:id>')
+    @token_required(id)
+    def changeUserInfo(id):
+        '''Route for change user info'''
+        data = request.json
+
+        u = User(
+            name=data['name'],
+            email=data['email'],
+            password=data['password'],
+            cpf=re.sub(r'[^0-9]', '', data['cpf']),
+            phone=re.sub(r'[^0-9]', '', data['phone'])
+        )
+
+        a = Address(
+            number=data['number'],
+            complement=data['complement'],
+            cep=re.sub(r'[^0-9]', '', data['cep']),
+            city=data['city']
+        )
+
+        if not is_valid_email(u.email):
+            raise 'Email não válido.'
+        
+        if not is_valid_cpf(u.cpf):
+            raise 'CPF não válido.'
+
+        if not is_valid_phone(u.phone):
+            raise 'Telefone não válido.'
+
+        if not is_valid_cep(a.cep):
+            raise 'CEP não válido.'
+        
+        for attr, val in vars(u).items():
+            srvc.update(attr, f'ID_User = {id}', val)
+
+        for attr, val in vars(a).items():
+            addrsrvc.update(attr, f'ID_User = {id}', val)
+        
+        return jsonify({'msg': f'{u.name} Atualizou suas informações.'})
 
     @bp.post('/users/signup')
     def SignUp():
